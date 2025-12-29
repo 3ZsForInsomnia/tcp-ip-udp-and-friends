792ce847eacb254cab9450f02b4c3d24e49284ed
Zachary Levine <zach@zjlevine.dev>
2025-12-28 19:01:35 -0500

    Working CLI setup

--- go/cmd/udp/main.go
+++ go/cmd/udp/main.go
@@ -11,43 +11,42 @@ import (
 
 func main() {
 	var command string
-	flag.StringVar(&command, "command", "help", "Command to execute: send or listen")
-	if command == "help" {
-		flag.PrintDefaults()
-	}
-	if command != "send" && command != "listen" && command != "help" {
+	flag.StringVar(&command, "c", "send", "Command to execute: send or listen")
+	if command != "send" && command != "listen" {
 		panic("Invalid command. Use 'send' or 'listen', or 'help' for usage.")
 	}
 
 	var input string
-	flag.StringVar(&input, "input", "Hello there, bitchfuck!", "Input string to send via UDP")
+	flag.StringVar(&input, "i", "Hello there, bitchfuck!", "Input string to send via UDP")
 	if command == "send" && input == "" {
 		panic("Input string cannot be empty when command is 'send'")
 	}
 	if command == "listen" {
-		fmt.Println("Input string is not used when command is 'listen'")
+		fmt.Println("Input string is not used when command is 'listen'. Did you mean to do this?")
 	}
 
 	var listenPort uint
-	flag.UintVar(&listenPort, "listenPort", 8080, "Port to listen on for UDP packets")
+	flag.UintVar(&listenPort, "l", 8080, "Port to listen on for UDP packets")
 	if !ports.IsValidPort(uint16(listenPort)) {
 		panic("Invalid listen port")
 	}
 	listenPortUint16 := uint16(listenPort)
 
 	var destPort uint
-	flag.UintVar(&destPort, "destPort", 8081, "Destination port to send UDP packets to")
+	flag.UintVar(&destPort, "d", 8081, "Destination port to send UDP packets to")
 	if !ports.IsValidPort(uint16(destPort)) {
 		panic("Invalid destination port")
 	}
 	destPortUint16 := uint16(destPort)
 
 	var logLevel string
-	flag.StringVar(&logLevel, "logLevel", "INFO", "Logging level (DEBUG, INFO, WARN, ERROR)")
+	flag.StringVar(&logLevel, "log", "INFO", "Logging level (INFO, WARN, ERROR)")
 	level, err := logger.GetLogLevelFromString(logLevel)
 	if err != nil {
 		panic(err)
 	}
+
+	flag.Parse()
 
 	inputBytes := []byte(input)
 
